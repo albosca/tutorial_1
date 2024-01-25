8e0cb6d5a53f3caa387ca0516da153cc84d29ffc
Alberto Boscá <alberto.bosca@upm.es>
2024-01-25 17:16:41 +0100

    removed extra code

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
  * >
  * > ✅ 1. Initialize the system \n
  * > ✅ 2. Initialize the LED GPIO \n
- * > ✅ 3. Infinite loop that toggles the LED value. The toggling period must be T_LED_MS \n
+ * > ✅ 3. Infinite loop that toggles the LED value. The toggling period must be BLINK_T_MS/2 \n
  * > ✅ 4. Even thought it never returns, it is good practice to return 0 at the end of the function.
  *
  * @return this function never returns.
